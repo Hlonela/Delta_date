135074cad37a95d03080a54e0009a9cbd80c3df2
Hlonela <hloni.mabhani@gmail.com>
2021-05-18 16:02:57 +0200

    first commit

---
+++ date_exercise.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timedelta, time
+
+#for days in range ():
+#dt = datetime (2021, 5, 18)
+#answer = dt+timedelta(days=7)
+#x=datetime.now()
+#print(answer)
+#print(x)
+
+init_time_for_now = datetime.now()
+#Printing initial date
+print("Initial date:", str(init_time_for_now))
+
+#Calculating future dates
+
+#Using a for loop
+
+
+
+
+
+x = 0
+
+
+while x < 10:
+    x = x+1
+    init_time_for_now = init_time_for_now + timedelta (days=7)
+    print(init_time_for_now)
+
